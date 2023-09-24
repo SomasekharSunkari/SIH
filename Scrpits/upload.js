1d1c43556c2b03ace3b20b9fc129a7f4e838a5ba
Sekhar <sunkarisekhar36@gmail.com>
2023-09-24 19:13:25 +0530

    Contract Nam has been changed in the Contract file

--- Scrpits/upload.js
+++ Scrpits/upload.js
@@ -1,11 +1,9 @@
 const Moralis = require("moralis").default;
 const fs = require("fs");
-
 async function uploadToIpfs() {
   await Moralis.start({
     apiKey: "QpR4EBjFJhIDiZw86ueWPd0rz4BCgbxRI9p8sYaiYZfoif6Jsm9aGrvboU7agNrX",
   });
-
   //2
   const uploadArray = [
     {
@@ -13,13 +11,10 @@ async function uploadToIpfs() {
       content: fs.readFileSync("./checked.png", { encoding: "base64" }),
     },
   ];
-
   //3̀
   const response = await Moralis.EvmApi.ipfs.uploadFolder({
     abi: uploadArray,
   });
-
   console.log(response.result);
 }
-
 uploadToIpfs();
